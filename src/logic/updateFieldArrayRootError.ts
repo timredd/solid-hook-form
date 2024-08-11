661ac592b2b1d1dd89cc94b455c73ac44f69a330
timredd <timredd13@gmail.com>
2024-08-11 11:38:02 -0400

    Add logic

---
+++ src/logic/updateFieldArrayRootError.ts
@@ -0,0 +1,20 @@
+import {
+  FieldError,
+  FieldErrors,
+  FieldValues,
+  InternalFieldName,
+} from '../types'
+import convertToArrayPayload from '../utils/convertToArrayPayload'
+import get from '../utils/get'
+import set from '../utils/set'
+
+export default <T extends FieldValues = FieldValues>(
+  errors: FieldErrors<T>,
+  error: Partial<Record<string, FieldError>>,
+  name: InternalFieldName,
+): FieldErrors<T> => {
+  const fieldArrayErrors = convertToArrayPayload(get(errors, name))
+  set(fieldArrayErrors, 'root', error[name])
+  set(errors, name, fieldArrayErrors)
+  return errors
+}
